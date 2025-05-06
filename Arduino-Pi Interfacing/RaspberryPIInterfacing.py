0603195a0efd7b7997cefa5a3458f8fe7588700b
Inesh Dey <ineshdey@hotmail.com>
2025-05-06 10:28:30 -0700

    updated code

--- Arduino-Pi Interfacing/RaspberryPIInterfacing.py
+++ Arduino-Pi Interfacing/RaspberryPIInterfacing.py
@@ -4,18 +4,17 @@ import time
 
 ser = serial.Serial('/dev/ttyACM0',9600) #starts up a serial at this serial port with this baud rate
 #TODO: find serial port for Arduino
-
+time.sleep(2)
 #reads data from the Serial
 #ser.readline()
 #writes data to serial.
 #ser.write()
-def turnOnPump(): ser.write(1)
-def turnOffPump(): ser.write(2)
-//would request for data to be received
-def receiveData(): ser.wrtie(3)
+def turnOnPump(): ser.write(b'1\r\n')
+def turnOffPump(): ser.write(b'2\r\n')
+def receiveData(): ser.write(b'3\r\n')
+
 while True:
-	turnOffPump();
-	time.wait(1)
-	receiveData();
-	time.wait(1000)
-	print(ser.readLine())
+	turnOffPump()
+	receiveData()
+	print(ser.readline())
+	time.sleep(1)
